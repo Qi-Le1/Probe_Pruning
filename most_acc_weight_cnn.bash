e91ddd9a4cb258f34f0f06cde57b6387a88042c4
qi <1099451900@qq.com>
2024-02-26 12:16:14 -0600

    update

--- most_acc_weight_cnn.bash
+++
@@ -1,4 +0,0 @@
-mkdir /home/aanwar/le000288/Efficient_representation_inference/msiout
-sbatch msiout0CIFAR10_cnn_0.1_100_iid_fedavg_5_0_10_2_PQ_0.001_PQ.pbs --wait
-mkdir /home/aanwar/le000288/Efficient_representation_inference/msiout
-sbatch msiout0CIFAR10_cnn_0.1_100_iid_fedavg_5_0_10_2_PQ_0.001_inter.pbs --wait
--- old/optimizer/api.py
+++
@@ -1,7 +0,0 @@
-from .optimizer import create_optimizer
-from .scheduler import create_scheduler
-
-__all__ = [
-    'create_optimizer',
-    'create_scheduler'
-]
