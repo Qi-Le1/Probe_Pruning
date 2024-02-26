088aaeee5046e1d3d21a77e9fb0d1d704ca84840
qi <1099451900@qq.com>
2024-02-26 16:53:52 -0600

    merge

--- src/msi_test_model_clm.bash
+++ src/msi_test_model_clm.bash
@@ -1,2 +1,4 @@
-bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.5_mag-probe0.9runningmeancalib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
-bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.6_mag-probe0.9runningmeancalib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
+bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.7_mag-fullinfprobe0.1calib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
+bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.7_mag-fullinfprobe0.1calib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
+bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.8_mag-fullinfprobe0.1calib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
+bash clm0wikitext-2v1_llama-2-7b_clm_10_128_0.8_mag-fullinfprobe0.1calib-probe-None-None+wikitest-all_gate-proj+up-proj+down-proj.sh --wait
